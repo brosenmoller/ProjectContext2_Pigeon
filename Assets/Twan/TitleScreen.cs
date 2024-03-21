49d3c76cce94c2e4b5c82c4142e070087061af21
reinderstwan <twan.reinders@student.hku.nl>
2024-03-21 11:24:14 +0100

    TitleScreen (NOT YET FUNCTIONAL)

--- Assets/Twan/TitleScreen.cs
+++ Assets/Twan/TitleScreen.cs
@@ -5,24 +5,16 @@ using UnityEngine.VFX;
 
 public class TitleScreen : MonoBehaviour
 {
-    public VisualEffect pigeonVFXL;
-    public VisualEffect pigeonVFXR;
-
     public AudioSource pigeonSound;
 
-    // Start is called before the first frame update
     void Start()
     {
-        pigeonVFXL.Stop();
-        pigeonVFXR.Stop();  
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)) {
-            pigeonVFXL.Play();
-            pigeonVFXR.Play();
             pigeonSound.Play();
         }
 
